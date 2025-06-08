eaf2d65a4a0a781916aa5d4a187f599359b03977
SansCaar <lamsalsskr@gmail.com>
2025-06-09 00:03:44 +0545

    ui:Chat section message segration complete

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -10,18 +10,44 @@ const ChatArea = ({}) => {
   const chat = useAtomValue(ChatAtom);
   const setChat = useSetAtom(ChatAtom);
 
-  const addMessage = ({ message, user, response }: { message: string; user: string, response?: string }) => {
+  const addMessage = ({
+    message,
+    user,
+    response,
+  }: {
+    message: string;
+    user: string;
+    response?: string;
+  }) => {
     setChat((prev) => {
       return {
         ...prev,
-        messages: [...prev.messages, { id: uuid(), message, userId: user, response }],
+        messages: [
+          ...prev.messages,
+          { id: uuid(), message, userId: user, response },
+        ],
       };
     });
   };
 
   return (
     <div className="h-full w-1/4 rounded-md flex flex-col">
-      <div className="flex-1 h-full">chatArea</div>
+      <div className="flex-col flex gap-4  h-full justify-end">
+        {chat.messages.map((message, index) => {
+          return (
+            <>
+              <div key={`message-${message.id}`} className="self-end w-fit bg-gray-300 rounded-md p-2 text-md text-gray-800">
+                {message.message}
+              </div>
+              {message.response && (
+                <div key={`response-${message.id}`} className=" w-full  rounded-md p-2 text-md text-gray-800">
+                  {message.response}
+                </div>
+              )}
+            </>
+          );
+        })}
+      </div>
       {/*  bottom input section with three buttons first mic second text input and third send */}
       <div className="w-full flex py-4 gap-2">
         <button className="h-full  aspect-square flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -35,6 +61,7 @@ const ChatArea = ({}) => {
               addMessage({
                 user: "GPT",
                 message: "Hello",
+                response: "Meowwwwwww"
               });
             }}
             className="h-full  aspect-square flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
