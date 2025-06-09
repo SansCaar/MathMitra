622e4cb49328fcdb01960671f0ec962021b3527e
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2025-06-09 23:17:18 +0545

    chore: added recent assignments for user

--- app/api/assignments/viewAll/route.ts
+++ app/api/assignments/viewAll/route.ts
@@ -66,6 +66,49 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
         body: assignmentData,
       });
     }
+    if (body.returnResBy == "studentId") {
+      if (!body.studentId) {
+        return NextResponse.json({
+          message: "Fields are Required",
+          status: "failed",
+          body: "ok",
+        });
+      }
+
+      console.log("ok");
+      const studentId = body.studentId;
+      const classes = await prisma.classes.findMany({});
+
+      // Type for student data in the JSON array
+      type StudentData = {
+        id: string;
+        [key: string]: any;
+      };
+
+      const studentClasses = classes.filter(
+        (classItem) =>
+          Array.isArray(classItem.studentId) &&
+          classItem.studentId.some((student: any) => student?.id === studentId)
+      );
+
+      console.log(studentClasses);
+      // Get assignments for all classes the student is part of
+      const assignments = await prisma.assignment.findMany({
+        where: {
+          classCode: {
+            in: studentClasses
+              .map((classItem) => classItem.classCode)
+              .filter((code): code is number => code !== null),
+          },
+        },
+      });
+      console.log(assignments);
+
+      return NextResponse.json({
+        status: "success",
+        data: assignments,
+      });
+    }
   } catch (err) {
     console.log(err);
     return NextResponse.json({
