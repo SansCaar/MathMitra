e48163a9c2db21f825c5c119e2ae197554646800
SansCaar <lamsalsskr@gmail.com>
2025-06-09 15:43:25 +0545

    fix: Student validation

--- app/(users)/layout.tsx
+++ app/(users)/layout.tsx
@@ -3,15 +3,28 @@
 import { TeacherHeader } from "@components/teacher/teacher-header";
 import { UserAtom } from "@src/atoms/UserAtom";
 import { useAtomValue } from "jotai";
+import { useEffect, useState } from "react";
 
 interface TeacherLayoutProps {
   children: React.ReactNode;
 }
 const layout = ({children}:TeacherLayoutProps) => {
+const [user, setUser] = useState<any>(null);
+  const checkLocalStorage = async () => {
+    const user = localStorage.getItem("user");
+    console.log(user);
+    if (!user) {
+      window.location.href = "/auth/login";
+      return
+      }
+    console.log(user);
+    setUser(JSON.parse(user));
+    }
+  useEffect(() => {
+    checkLocalStorage();
+  }, []);
+    
 
-  const user =useAtomValue(UserAtom);
-
-  if(!user) window.location.href="/auth/login";
 
   return (
     <div className="min-h-screen bg-slate-50">
