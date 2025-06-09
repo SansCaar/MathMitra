2110ceb5bfca809a209a73e0ebe058ecafb0dc2e
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2025-06-09 10:31:39 +0545

    feat: fixed env

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -18,6 +18,7 @@ export default function TeacherDashboard() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    alert(process.env.NEXT_PUBLIC_HEY);
     const fetchClasses = async () => {
       try {
         const response = await fetch("/api/classes/fetchClasses", {
